03a55dddaa249535d90e158ccab2bfee3ced4cef
guzmanrafael <rafael.guzman1609@gmail.com>
2021-05-31 12:05:17 -0700

    Update

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,8 @@
 import social_logo from "../img/main_social.png";
 import recuerdame_logo from "../img/main_recuerdame.png";
+import cotizacion_logo from "../img/cotizacion_main.png";
+import taller_logo from "../img/taller_main.png";
+
 import barberapp_logo from "../img/main_barberapp.png";
 import barberweb_logo from "../img/main_barberweb.png";
 import barberweb_logo_1 from "../img/barberweb_1.png";
@@ -21,6 +24,21 @@ import barberapp_logo_8 from "../img/barberapp_8.jpeg";
 import barberapp_logo_9 from "../img/barberapp_9.jpeg";
 import barberapp_logo_10 from "../img/barberapp_10.jpeg";
 
+import cotizacion_logo_1 from "../img/cotizacion1.png";
+import cotizacion_logo_2 from "../img/cotizacion2.png";
+import cotizacion_logo_3 from "../img/cotizacion3.png";
+import cotizacion_logo_4 from "../img/cotizacion4.png";
+import cotizacion_logo_5 from "../img/cotizacion5.png";
+import cotizacion_logo_6 from "../img/cotizacion6.png";
+import cotizacion_logo_7 from "../img/cotizacion7.png";
+import cotizacion_logo_8 from "../img/cotizacion8.png";
+import cotizacion_logo_9 from "../img/cotizacion9.png";
+import cotizacion_logo_10 from "../img/cotizacion10.png";
+import cotizacion_logo_11 from "../img/cotizacion11.png";
+import cotizacion_logo_12 from "../img/cotizacion12.png";
+
+import paginataller_logo_1 from "../img/paginataller.png";
+
 import social_logo_1 from "../img/social_1.png";
 import social_logo_2 from "../img/social_2.png";
 import social_logo_3 from "../img/social_3.png";
@@ -31,160 +49,212 @@ import recuerdame_logo_3 from "../img/recuerdame_3.png";
 import myportfolio from "../img/portfolio_1.png";
 import myportfolio2 from "../img/portfolio_2.png";
 
-
-
 export const proyectsData = [
-    {
-        id: "1",
-        name: "Barber Chein Web",
-        description: "Una Aplicación Web de administración de la barbería Barber Chein",
-        main_photo: barberweb_logo,
-        technologies: [
-            "React Js",
-            "Firebase",
-            "Tailwind css"
-        ],
-        github_link: false,
-        link: false,
-        photos:
-            [
-                {
-                    original: barberweb_logo_1,
-                },
-                {
-                    original: barberweb_logo_2,
-                },
-                {
-                    original: barberweb_logo_3,
-                },
-                {
-                    original: barberweb_logo_4,
-                },
-                {
-                    original: barberweb_logo_5,
-                },
-                {
-                    original: barberweb_logo_6,
-                },
-                {
-                    original: barberweb_logo_7,
-                }
-            ],
-        type: "web"
-    },
-    {
-        id: "2",
-        name: "Barber Chein App",
-        description: "Una Aplicación Móvil para los clientes de la barbería Barber Chein, en el cual pueden agendar citas.",
-        main_photo: barberapp_logo,
-        technologies: [
-            "React Native",
-            "Firebase",
-        ],
-        github_link: false,
-        link: false,
-        photos: [
-            {
-                original: barberapp_logo_1,
-            },
-            {
-                original: barberapp_logo_2,
-            },
-            {
-                original: barberapp_logo_3,
-            },
-            {
-                original: barberapp_logo_4,
-            },
-            {
-                original: barberapp_logo_5,
-            },
-            {
-                original: barberapp_logo_6,
-            },
-            {
-                original: barberapp_logo_7,
-            },
-            {
-                original: barberapp_logo_8,
-            },
-            {
-                original: barberapp_logo_9,
-            },
-            {
-                original: barberapp_logo_10,
-            }
-        ],
-        type: "mobile"
-    },
-    {
-        id: "3",
-        name: "Rafael Guzmán Portafolio",
-        description: "Es un sitio web donde puedes encontrar proyectos de mi persona",
-        main_photo: myportfolio2,
-        technologies: [
-            "React Js",
-            "Material Ui",
-        ],
-        github_link: "",
-        link: "https://rafaelguzman.netlify.app/",
-        photos: [
-            {
-                original: myportfolio,
-            },
-        ],
-        type: "web"
-    },
-    {
-        id: "4",
-        name: "Social-Rif",
-        description: "Proyecto escolar con mis compañeros Ruben Velazquez, Cesar Romero y yo Rafael Guzmán. Es una red social para subir publicaciones agregando una imagen, y reaccionar a publicaciones de otros usuarios.",
-        main_photo: social_logo,
-        technologies: [
-            "Node Js",
-            "Express",
-            "MongoDB",
-            "React Js",
-            "Material Ui"
-        ],
-        github_link: "https://github.com/guzmanrafael/social-app",
-        link: "https://socia-rif.herokuapp.com/",
-        photos: [
-            {
-                original: social_logo_1,
-            },
-            {
-                original: social_logo_2,
-            },
-            {
-                original: social_logo_3
-            }
-        ],
-        type: "web"
-    },
-    {
-        id: "5",
-        name: "Recuérdame",
-        description: "Proyecto escolar con mis compañeros Ruben Velazquez, Cesar Romero, Damian Bojorquez y yo Rafael Guzmán. Es Una Aplicación Web para administrar notas.",
-        main_photo: recuerdame_logo,
-        technologies: [
-            "React Js",
-            "Firebase",
-            "Material Ui"
-        ],
-        github_link: "https://github.com/guzmanrafael/note-app",
-        link: "https://react-app-cursos-d6169.web.app/",
-        photos: [
-            {
-                original: recuerdame_logo_1,
-            },
-            {
-                original: recuerdame_logo_2,
-            },
-            {
-                original: recuerdame_logo_3
-            }
-        ],
-        type: "web"
-    }
-]
+  {
+    id: "1",
+    name: "Sistema de cotización",
+    description:
+      "Practicas profesionales realizado con Cesar Romero, para el negocio Taller El Marro.",
+    main_photo: cotizacion_logo,
+    technologies: [
+      "Node Js con Express",
+      "Postgresql",
+      "React Js",
+      "Sequelize",
+    ],
+    github_link: "https://github.com/guzmanrafael/taller-el-marro-backend",
+    link: false,
+    photos: [
+      {
+        original: cotizacion_logo_1,
+      },
+      {
+        original: cotizacion_logo_2,
+      },
+      {
+        original: cotizacion_logo_3,
+      },
+      {
+        original: cotizacion_logo_4,
+      },
+      {
+        original: cotizacion_logo_5,
+      },
+      {
+        original: cotizacion_logo_6,
+      },
+      {
+        original: cotizacion_logo_7,
+      },
+      {
+        original: cotizacion_logo_8,
+      },
+      {
+        original: cotizacion_logo_9,
+      },
+      {
+        original: cotizacion_logo_10,
+      },
+      {
+        original: cotizacion_logo_11,
+      },
+      {
+        original: cotizacion_logo_12,
+      },
+    ],
+    type: "web",
+  },
+  {
+    id: "2",
+    name: "Página Taller El Marro",
+    description:
+      "Practicas profesionales realizado con Cesar Romero, para el negocio Taller El Marro.",
+    main_photo: taller_logo,
+    technologies: ["React Js"],
+    github_link: false,
+    link: "https://tallerelmarro.netlify.app/",
+    photos: [
+      {
+        original: paginataller_logo_1,
+      },
+    ],
+    type: "web",
+  },
+  {
+    id: "3",
+    name: "Barber Chein Web",
+    description:
+      "Una Aplicación Web de administración de la barbería Barber Chein",
+    main_photo: barberweb_logo,
+    technologies: ["React Js", "Firebase", "Tailwind css"],
+    github_link: false,
+    link: false,
+    photos: [
+      {
+        original: barberweb_logo_1,
+      },
+      {
+        original: barberweb_logo_2,
+      },
+      {
+        original: barberweb_logo_3,
+      },
+      {
+        original: barberweb_logo_4,
+      },
+      {
+        original: barberweb_logo_5,
+      },
+      {
+        original: barberweb_logo_6,
+      },
+      {
+        original: barberweb_logo_7,
+      },
+    ],
+    type: "web",
+  },
+  {
+    id: "4",
+    name: "Barber Chein App",
+    description:
+      "Una Aplicación Móvil para los clientes de la barbería Barber Chein, en el cual pueden agendar citas.",
+    main_photo: barberapp_logo,
+    technologies: ["React Native", "Firebase"],
+    github_link: false,
+    link: false,
+    photos: [
+      {
+        original: barberapp_logo_1,
+      },
+      {
+        original: barberapp_logo_2,
+      },
+      {
+        original: barberapp_logo_3,
+      },
+      {
+        original: barberapp_logo_4,
+      },
+      {
+        original: barberapp_logo_5,
+      },
+      {
+        original: barberapp_logo_6,
+      },
+      {
+        original: barberapp_logo_7,
+      },
+      {
+        original: barberapp_logo_8,
+      },
+      {
+        original: barberapp_logo_9,
+      },
+      {
+        original: barberapp_logo_10,
+      },
+    ],
+    type: "mobile",
+  },
+  {
+    id: "5",
+    name: "Rafael Guzmán Portafolio",
+    description:
+      "Es un sitio web donde puedes encontrar proyectos de mi persona",
+    main_photo: myportfolio2,
+    technologies: ["React Js", "Material Ui"],
+    github_link: "https://github.com/guzmanrafael/my-portfolio",
+    link: "https://rafaelguzman.netlify.app/",
+    photos: [
+      {
+        original: myportfolio,
+      },
+    ],
+    type: "web",
+  },
+  {
+    id: "6",
+    name: "Social-Rif",
+    description:
+      "Proyecto escolar con mis compañeros Ruben Velazquez, Cesar Romero y yo Rafael Guzmán. Es una red social para subir publicaciones agregando una imagen, y reaccionar a publicaciones de otros usuarios.",
+    main_photo: social_logo,
+    technologies: ["Node Js", "Express", "MongoDB", "React Js", "Material Ui"],
+    github_link: "https://github.com/guzmanrafael/social-app",
+    link: "https://socia-rif.herokuapp.com/",
+    photos: [
+      {
+        original: social_logo_1,
+      },
+      {
+        original: social_logo_2,
+      },
+      {
+        original: social_logo_3,
+      },
+    ],
+    type: "web",
+  },
+  {
+    id: "7",
+    name: "Recuérdame",
+    description:
+      "Proyecto escolar con mis compañeros Ruben Velazquez, Cesar Romero, Damian Bojorquez y yo Rafael Guzmán. Es Una Aplicación Web para administrar notas.",
+    main_photo: recuerdame_logo,
+    technologies: ["React Js", "Firebase", "Material Ui"],
+    github_link: "https://github.com/guzmanrafael/note-app",
+    link: "https://react-app-cursos-d6169.web.app/",
+    photos: [
+      {
+        original: recuerdame_logo_1,
+      },
+      {
+        original: recuerdame_logo_2,
+      },
+      {
+        original: recuerdame_logo_3,
+      },
+    ],
+    type: "web",
+  },
+];
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Icon, Paper, Typography } from "@material-ui/core";
@@ -7,194 +6,220 @@ import { motion } from "framer-motion";
 import { animationOne, transition } from "../animations";
 
 const useStyle = makeStyles({
-    root: {
-        flexGrow: 1,
-        position: "relative",
-        top: "6rem",
-        margin: "0rem 3rem",
-        [theme.breakpoints.down("xs")]: {
-            top: "4rem",
-            margin: "0rem 2rem",
-        },
-
+  root: {
+    flexGrow: 1,
+    position: "relative",
+    top: "6rem",
+    margin: "0rem 3rem",
+    [theme.breakpoints.down("xs")]: {
+      top: "4rem",
+      margin: "0rem 2rem",
     },
-    paper: {
-        padding: theme.spacing(3),
-        textAlign: 'left',
-        background: "transparent",
-        boxShadow: 'none',
-        marginTop: "2rem",
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: "left",
+    background: "transparent",
+    boxShadow: "none",
+    marginTop: "2rem",
+  },
+  title: {
+    color: "#F0F0F0",
+    fontSize: "1.4rem",
+  },
+  aboutMe: {
+    color: "#F0F0F0",
+    fontSize: "1.1rem",
+    marginTop: "1rem",
+    lineHeight: "200%",
+  },
+  paperSkills: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  skills: {
+    padding: theme.spacing(5),
+    background: "#303030",
+    marginTop: "1rem",
+    textAlign: "center",
+    color: "white",
+    "&:hover": {
+      color: "black",
+      background: "#282828",
     },
-    title: {
-        color: "#F0F0F0",
-        fontSize: "1.4rem"
+  },
+  iconSvg: {
+    [theme.breakpoints.only("xs")]: {
+      height: "100px",
+      width: "100px",
     },
-    aboutMe: {
-        color: "#F0F0F0",
-        fontSize: "1.1rem",
-        marginTop: "1rem",
-        lineHeight: "200%"
+    [theme.breakpoints.only("sm")]: {
+      height: "80px",
+      width: "80px",
     },
-    paperSkills: {
-        display: "flex",
-        justifyContent: "space-around",
+    [theme.breakpoints.only("md")]: {
+      height: "90px",
+      width: "90px",
     },
-    skills: {
-        padding: theme.spacing(5),
-        background: "#303030",
-        marginTop: "1rem",
-        textAlign: "center",
-        color: "white",
-        "&:hover": {
-            color: "black",
-            background: "#282828"
-        },
+    [theme.breakpoints.only("lg")]: {
+      height: "100px",
+      width: "100px",
     },
-    iconSvg: {
-        [theme.breakpoints.only("xs")]: {
-            height: "100px",
-            width: "100px",
-        },
-        [theme.breakpoints.only("sm")]: {
-            height: "80px",
-            width: "80px",
-        },
-        [theme.breakpoints.only("md")]: {
-            height: "90px",
-            width: "90px",
-        },
-        [theme.breakpoints.only("lg")]: {
-            height: "100px",
-            width: "100px",
-        },
-        [theme.breakpoints.only("xl")]: {
-            height: "100px",
-            width: "100px",
-        },
-    }
+    [theme.breakpoints.only("xl")]: {
+      height: "100px",
+      width: "100px",
+    },
+  },
 });
 
 export const About = () => {
-    const classes = useStyle();
+  const classes = useStyle();
 
-    return (
-        <motion.div initial='out' animate='in' exit='out' variants={animationOne}
-            transition={transition}>
-            <div className={classes.root}>
-                <Grid container spacing={5}>
-                    <Grid item xs={12} sm={12} md={5}>
-                        <Paper className={classes.paper}>
-                            <Typography align="left" className={classes.title}>
-                                ¿Quien Soy?
-                            </Typography>
-                            <Typography align="justify" className={classes.aboutMe}>
-                                Mi nombre es Rafael Ivan Guzmán Ramírez, soy de México, y actualmente estoy estudiando Ingeniería en Software en el Instituto Tecnológico de Sonora. Me gusta y me apasiona el Desarrollo Web Backend &     Frontend.
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={12} md={7}>
-                        <Paper className={classes.paper}>
-                            <Typography align="center" className={classes.title}>
-                                Mis Habilidades
-                            </Typography>
-                            <Grid container spacing={2} className={classes.paperSkills}>
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/nodejs.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            NODE
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/express.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            EXPRESS
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/react.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            REACT
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/mongodb.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            MONGODB
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/firebase.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            FIREBASE
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/git-icon.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            GIT
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/material-ui.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            MATERIAL UI
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/html-5.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            HTML
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-                                <Grid item xs={12} sm={4} md={4}>
-                                    <Paper className={classes.skills}>
-                                        <Icon>
-                                            <img src="https://cdn.svgporn.com/logos/css-3.svg" className={classes.iconSvg} alt="Node" />
-                                        </Icon>
-                                        <Typography align="center">
-                                            CSS
-                                        </Typography>
-                                    </Paper>
-                                </Grid>
-                            </Grid>
-                        </Paper>
-                    </Grid>
+  return (
+    <motion.div
+      initial="out"
+      animate="in"
+      exit="out"
+      variants={animationOne}
+      transition={transition}
+    >
+      <div className={classes.root}>
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={12} md={5}>
+            <Paper className={classes.paper}>
+              <Typography align="left" className={classes.title}>
+                ¿Quien Soy?
+              </Typography>
+              <Typography align="justify" className={classes.aboutMe}>
+                Mi nombre es Rafael Ivan Guzmán Ramírez, soy de México, y
+                actualmente estoy estudiando Ingeniería en Software en el
+                Instituto Tecnológico de Sonora. Me gusta y me apasiona el
+                Desarrollo Web Backend & Frontend.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={12} md={7}>
+            <Paper className={classes.paper}>
+              <Typography align="center" className={classes.title}>
+                Mis Habilidades
+              </Typography>
+              <Grid container spacing={2} className={classes.paperSkills}>
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/nodejs.svg"
+                        className={classes.iconSvg}
+                        alt="Node"
+                      />
+                    </Icon>
+                    <Typography align="center">NODE</Typography>
+                  </Paper>
                 </Grid>
-            </div>
-        </motion.div>
-    );
-}
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/react.svg"
+                        className={classes.iconSvg}
+                        alt="React"
+                      />
+                    </Icon>
+                    <Typography align="center">REACT</Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/mongodb.svg"
+                        className={classes.iconSvg}
+                        alt="MongoDB"
+                      />
+                    </Icon>
+                    <Typography align="center">MONGODB</Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/firebase.svg"
+                        className={classes.iconSvg}
+                        alt="Firebase"
+                      />
+                    </Icon>
+                    <Typography align="center">FIREBASE</Typography>
+                  </Paper>
+                </Grid>
+
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/git-icon.svg"
+                        className={classes.iconSvg}
+                        alt="Git"
+                      />
+                    </Icon>
+                    <Typography align="center">GIT</Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/postgresql.svg"
+                        className={classes.iconSvg}
+                        alt="Postgresql"
+                      />
+                    </Icon>
+                    <Typography align="center">POSTGRESQL</Typography>
+                  </Paper>
+                </Grid>
+
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/mysql.svg"
+                        className={classes.iconSvg}
+                        alt="Mysql"
+                      />
+                    </Icon>
+                    <Typography align="center">MYSQL</Typography>
+                  </Paper>
+                </Grid>
+
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/php.svg"
+                        className={classes.iconSvg}
+                        alt="Php"
+                      />
+                    </Icon>
+                    <Typography align="center">PHP</Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4}>
+                  <Paper className={classes.skills}>
+                    <Icon>
+                      <img
+                        src="https://cdn.svgporn.com/logos/python.svg"
+                        className={classes.iconSvg}
+                        alt="Python"
+                      />
+                    </Icon>
+                    <Typography align="center">PYTHON</Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </motion.div>
+  );
+};
